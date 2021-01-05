5b54b76bc69966d0fc631036d6461e4f712a2c99
washgo <38931933+washgo@users.noreply.github.com>
2021-01-05 11:40:38 +0800

    update service.py

--- service.py
+++ service.py
@@ -1,10 +1,10 @@
 import fileoperate
 
-def create_service(name, subid, service_name, service_port, service_password):
+def create_service(platform_name, name, subid, service_name, service_port, service_password):
     '''
     创建服务
     '''
-    service_data = read_service_config(name)
+    service_data = read_service_config(platform_name, name)
     service_item = {}
     service_item['subid'] = subid
     service_item['service_port'] = service_port
@@ -12,17 +12,17 @@ def create_service(name, subid, service_name, service_port, service_password):
     service_item['service_list'] = service_name.upper()
     service_item['services'] = {}
     service_data[subid] = service_item
-    write_service_config(name, service_data)
+    write_service_config(platform_name, name, service_data)
     return True
 
-def read_service_config(name):
+def read_service_config(platform_name, name):
     '''
     读取service配置数据
     '''
-    return fileoperate.read_server_service_file(name)
+    return fileoperate.read_server_service_file(platform_name, name)
 
-def write_service_config(name, data):
+def write_service_config(platform_name, name, data):
     '''
     写入service配置数据
     '''
-    return fileoperate.write_server_service_file(name, data)
+    return fileoperate.write_server_service_file(platform_name, name, data)
