c9ede801c13933f27df70f01a9b2c83bd347155b
washgo <38931933+washgo@users.noreply.github.com>
2021-01-05 11:37:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
 import json, sys
 import log, fileoperate
 
-def exist_apikey(name):
+def exist_apikey(platform_name, name):
     '''
     判断是否存在api_key
     '''
-    api_key = fileoperate.read_api_file(name)
+    api_key = fileoperate.read_api_file(platform_name, name)
     if api_key not in '':
         return True
     return False
 
-def modify_apikey(name, api_key):
+def modify_apikey(platform_name, name, api_key):
     '''
     修改api_key
     '''
-    return fileoperate.write_api_file(name, api_key)
+    return fileoperate.write_api_file(platform_name, name, api_key)
 
-def get_apikeynames():
+def get_apikeynames(platform_name):
     '''
     获取api_key对应的name
     '''
-    return fileoperate.read_apikeyname_file()
+    return fileoperate.read_apikeyname_file(platform_name)
 
 def get_services():
     '''
@@ -28,11 +28,11 @@ def get_services():
     '''
     return fileoperate.read_services_file()
 
-def get_apikey(name):
+def get_apikey(platform_name, name):
     '''
     获取api_key
     '''
-    return fileoperate.read_api_file(name)
+    return fileoperate.read_api_file(platform_name, name)
 
 def create_service_config(config_data):
     '''
