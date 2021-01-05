c34ef38b03f448e8c9c263d39970a3c1af11bfa9
washgo <38931933+washgo@users.noreply.github.com>
2021-01-05 11:38:29 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import requests
 import config, fileoperate, service
 import json, socket
-import paramiko, asyncio, time
+import time
 
-def get_servers(name):
+def get_servers(platform_name, name):
     servers = None
     # local_servers = read_config()
-    api_key = config.get_apikey(name)
+    api_key = config.get_apikey(platform_name, name)
     headers = {
         'API-Key':api_key
     }
@@ -14,27 +14,27 @@ def get_servers(name):
     if r.status_code == 200:
         servers = json.loads(r.text)
     if servers is not None:
-        write_config(name, servers)
+        write_config(platform_name, name, servers)
     return servers
 
-def get_server_by_subid(name, subid):
+def get_server_by_subid(platform_name, name, subid):
     '''
     根据subid获取对应的server的信息
     '''
-    api_key = config.get_apikey(name)
+    api_key = config.get_apikey(platform_name, name)
     headers = {
         'API-Key':api_key
     }
     r = requests.get('https://api.vultr.com/v1/server/list?SUBID=%s'%subid,headers=headers)
     if r.status_code == 200:
-        local_servers = read_config(name)
+        local_servers = read_config(platform_name, name)
         server_item = json.loads(r.text)
         local_servers[subid] = server_item
-        write_config(name, local_servers)
+        write_config(platform_name, name, local_servers)
         return server_item
     return None
 
-def create_server(name, region_dcid, plan_id, osid, project_name, server_num):
+def create_server(platform_name, name, region_dcid, plan_id, osid, project_name, server_num):
     api_key = config.get_apikey(name)
     headers = {
         'API-Key':api_key
@@ -50,15 +50,15 @@ def create_server(name, region_dcid, plan_id, osid, project_name, server_num):
     if r.status_code == 200:
         new_server = json.loads(r.text)
         subid = new_server['SUBID']
-        server_item = get_server_by_subid(name, subid)
+        server_item = get_server_by_subid(platform_name, name, subid)
         return subid
     return None
 
-def delete_server(name, subid):
+def delete_server(platform_name, name, subid):
     '''
     删除server
     '''
-    api_key = config.get_apikey(name)
+    api_key = config.get_apikey(platform_name, name)
     headers = {
         'API-Key':api_key
     }
@@ -67,43 +67,43 @@ def delete_server(name, subid):
     }
     r = requests.post('https://api.vultr.com/v1/server/destroy',headers=headers, data=data)
     if r.status_code == 200:
-        data = read_config(name)
+        data = read_config(platform_name, name)
         data.pop(subid)
-        write_config(name, data)
-        ss_data = service.read_service_config(name)
+        write_config(platform_name, name, data)
+        ss_data = service.read_service_config(platform_name, name)
         if subid in ss_data.keys():
             ss_data.pop(subid)
-            service.write_service_config(name, ss_data)
+            service.write_service_config(platform_name, name, ss_data)
         return True
     return False
 
-def start_server(name, subid):
+def start_server(platform_name, name, subid):
     '''
     开启server
     成功返回True，失败返回False
     '''
-    return execute_server(name, subid, 'start')
+    return execute_server(platform_name, name, subid, 'start')
 
-def stop_server(name, subid):
+def stop_server(platform_name, name, subid):
     '''
     关闭server
     成功返回True，失败返回False
     '''
-    return execute_server(name, subid, 'halt')
+    return execute_server(platform_name, name, subid, 'halt')
 
-def reboot_server(name, subid):
+def reboot_server(platform_name, name, subid):
     '''
     重启server
     成功返回True，失败返回False
     '''
-    return execute_server(name, subid, 'reboot')
+    return execute_server(platform_name, name, subid, 'reboot')
 
-def execute_server(name, subid, action):
+def execute_server(platform_name, name, subid, action):
     '''
     执行server
     成功返回True，失败返回False
     '''
-    api_key = config.get_apikey(name)
+    api_key = config.get_apikey(platform_name, name)
     headers = {
         'API-Key':api_key
     }
@@ -112,20 +112,20 @@ def execute_server(name, subid, action):
     }
     r = requests.post('https://api.vultr.com/v1/server/%s'%action,headers=headers, data=data)
     if r.status_code == 200:
-        server_data = read_config(name)
+        server_data = read_config(platform_name, name)
         if action == 'halt':
             server_data[subid]['power_status'] = 'stopped'
         elif action == 'reboot' or action == 'start':
             server_data[subid]['power_status'] = 'running'
-        write_config(name, server_data)
+        write_config(platform_name, name, server_data)
         return True
     return False
 
-def write_config(name, data):
+def write_config(platform_name, name, data):
     '''
     写入数据
     '''
-    return fileoperate.write_server_file(name, data)
+    return fileoperate.write_server_file(platform_name, name, data)
 
 def insert_service(name, subid, service_name, service_generation):
     '''
@@ -172,11 +172,11 @@ def update_service_list(name, subid, service_name):
             server_item['service_list'] = service_list + ',' + service_name
     service.write_service_config(name, data)
 
-def read_config(name):
+def read_config(platform_name, name):
     '''
     读取数据
     '''
-    return fileoperate.read_server_file(name)
+    return fileoperate.read_server_file(platform_name, name)
 
 def clear_config():
     '''
