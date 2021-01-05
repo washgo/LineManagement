a24ee293fdc0c825b1b1e490b6f29b60a6976962
washgo <38931933+washgo@users.noreply.github.com>
2021-01-05 11:39:24 +0800

    Update operatingsystem.py

--- operatingsystem.py
+++ operatingsystem.py
@@ -2,37 +2,37 @@ import json, os
 import requests
 import fileoperate
 
-def get_os(name):
+def get_os(platform_name, name):
     data = None
     r = requests.get('https://api.vultr.com/v1/os/list')
     if r.status_code == 200:
         data = json.loads(r.text)
     if data is not None:
-        write_config(name, data)
+        write_config(platform_name, name, data)
     return data
 
-def exist_os(name):
+def exist_os(platform_name, name):
 	'''
 	判断是否存在os
 	'''
 	data = {}
 
-	data = fileoperate.read_os_file(name)
+	data = fileoperate.read_os_file(platform_name, name)
 	if len(data) > 0:
 		return True
 	return False
 
-def write_config(name, data):
+def write_config(platform_name, name, data):
 	'''
 	写入数据
 	'''
-	fileoperate.write_os_file(name, data)
+	fileoperate.write_os_file(platform_name, name, data)
 
-def read_config(name):
+def read_config(platform_name, name):
 	'''
 	读取数据
 	'''
-	return fileoperate.read_os_file(name)
+	return fileoperate.read_os_file(platform_name, name)
 
 def clear_config(name):
 	'''
